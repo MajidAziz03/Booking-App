83e61e930836e044d12ccbb770909871bf1ddd6d
Majid Aziz <azizmajid34@gmail.com>
2023-03-03 13:42:47 +0500

    Completed Navbar

--- src/Components/navbar/Navbar.jsx
+++ src/Components/navbar/Navbar.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
-import './navbar.scss';
+import './navbar.scss'
 
 const Navbar = () => {
     return (
         <div className='navbar'>
-            
+            <div className="container">
+                <div className="left">
+                    <span>Majid Booking</span>
+                </div>
+                <div className="right">
+                    <button>Login</button>
+                    <button>Singup</button>
+                </div>
+            </div>
         </div>
     )
 }
