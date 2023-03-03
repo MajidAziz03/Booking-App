188a145133be5c9bb412f2b6ef3fcad7183af74a
Majid Aziz <azizmajid34@gmail.com>
2023-03-03 13:21:26 +0500

    Initial Commit and created folder structure

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../../Components/navbar/Navbar';
+import './home.scss';
+
+const Home = () => {
+    return (
+        <>
+            <Navbar />
+        </>
+    )
+}
+
+export default Home
